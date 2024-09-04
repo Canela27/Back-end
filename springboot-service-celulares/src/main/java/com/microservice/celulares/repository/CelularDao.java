67d4a36111bfbdb76161f0557ef16b2138cdf7d7
Canela27 <diegogzz371@gmail.com>
2024-09-04 15:59:17 -0600

    Video 2
    Se creo el servicio de celulares

---
+++ springboot-service-celulares/src/main/java/com/microservice/celulares/repository/CelularDao.java
@@ -0,0 +1,8 @@
+package com.microservice.celulares.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import com.microservice.celulares.entity.Celular;
+
+public interface CelularDao extends CrudRepository<Celular, Long> {
+
+}
---
+++ springboot-service-celulares/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO celulares (name, marca, created_at) VALUES ('Samsung', '521', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Samsung', '522', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Samsung', '523', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Apple', 'Iphone 14', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Apple', 'Iphone 15', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Xiamoi', 'Mi', NOW());
